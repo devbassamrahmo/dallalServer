eecddddd4d9b527d86e8cf9c42f4b8b000524c35
devbassamrahmo <bassam.rahmo22@gmail.com>
2025-03-01 02:20:06 +0300

    x2

--- models/Ad.js
+++ models/Ad.js
@@ -8,7 +8,7 @@ const adSchema = new mongoose.Schema(
     category: {
       type: String,
       required: true,
-      enum: ["car", "bike", "real_estate", "electronics", "furniture", "education", "services", "pets", "jobs", "others"],
+      enum: ["car", "bike", "real_estate", "electronics", "furniture", "education", "services", "pets", "jobs", "parties" ,"others"],
     },
     priceSYP: { type: Number, required: true },
     priceUSD: { type: Number, required: true },
