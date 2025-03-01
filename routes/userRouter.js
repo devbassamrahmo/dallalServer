5583e5fb0ea652f2c549807d3713c1e770b0c045
devbassamrahmo <bassam.rahmo22@gmail.com>
2025-03-01 14:35:20 +0300

    x3

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const { protect , isAdmin} = require("../middlewares/authMiddleware");
-
+  
 const {
   registerUser,
   loginUser,
