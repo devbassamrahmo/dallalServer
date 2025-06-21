2cc66ea81cdc0ccccf87db7270f6550823380ea2
devbassamrahmo <bassam.rahmo22@gmail.com>
2025-06-21 16:08:41 +0300

    approveAllFix

--- routes/adRouter.js
+++ routes/adRouter.js
@@ -4,6 +4,8 @@ const { protect , isAdmin} = require("../middlewares/authMiddleware");
 const { createAd, getAllAds, getAdById, deleteAd , refreshAd , approveAd , getUserAds , deleteByAdmin , getAllAdsAdmin , getPendingPosts , approveAll , rejectAll , updateAd} = require("../controllers/adController");
 const upload = require("../middlewares/multer");
 
+router.put('/approve-all' , protect , isAdmin , approveAll)
+router.put('/reject-all' , protect , isAdmin , rejectAll)
 // ✅ Public Routes
 router.get("/", getAllAds);
 router.get("/my-ads", protect, getUserAds);
@@ -19,8 +21,6 @@ router.get('/find/get-pending' , protect , isAdmin , getPendingPosts);
 router.put("/:id", protect, upload.array("images", 5), updateAd);
 
 //approve and reject all
-router.put('/approve-all' , protect , isAdmin , approveAll)
-router.put('/reject-all' , protect , isAdmin , rejectAll)
 // ✅ Admin Routes
 router.put("/:id/approve", protect, isAdmin, approveAd);
 
